d9bfc7876a5509bd5e8947f36f635df7f18f639e
Jomajiayang <majiayang@zuozh.com>
2017-04-12 18:20:39 +0800

    记录jenkins groovy

--- jenkins/shell.sh
+++ jenkins/shell.sh
@@ -9,4 +9,22 @@ for project in $projects
 do  
 	echo  ------------  now publish $project ----------------------\n
     python /var/jenkins_home/push.py $project $evn
-done  
+done
+
+
+
+# groovy
+
+node{
+env.build_name = build_name
+env.release_branch = release_branch
+def build_names = build_name.split(",")
+for(i=0;i<build_names.size();i++){
+        def job = build_names[i]
+        def job_name = job+'-deploy'
+        def git_url = 'git@gitlab.lark.wiki:nbgold/'+job+'.git'
+        println "start build:"+job +'  '+release_branch
+        build job: job, parameters: [string(name: 'git_url', value: git_url), string(name: 'git_name', value: job), string(name: 'release_branch', value: release_branch)]
+        //build job: 'cms', parameters: [string(name: 'git_url', value: 'git@gitlab.lark.wiki:nbgold/cms.git'), string(name: 'git_name', value: 'cms'), string(name: 'release_branch', value: 'master')]
+}
+}

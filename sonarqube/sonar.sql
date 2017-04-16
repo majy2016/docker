a07c9ecb968c948f5b775e4f259c69f25cf38750
majy <372840680@qq.com>
2017-04-17 00:48:34 +0800

    增加sonar sql

---
+++ sonarqube/sonar.sql
@@ -0,0 +1,6 @@
+-- 初始化脚本
+CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'sonar' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar';
+FLUSH PRIVILEGES;
